7a51fca34d98b58ef2f71ddf3e1f769be8e3d948
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 21:07:41 +0200

    commit on 'master'

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+#Format de la liste exo:
+# [[nom, image(boolean), image(path en string), texte, points], ...]
 
 app = Flask(__name__)
-  
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/projectionvectorielle')
+@app.route('/c/projectionvectorielle')
 def projvect():
     return render_template('cours/projectionvectorielle.html')
-@app.route('/exercices') 
+@app.route('/ex/newton') 
 def exercices():
-    return render_template('exercices/newton.html')
+    n = request.args.get('n', type=int)
+    print(n)
+    return render_template('exercices/newton.html', 
+                           exo =  [["Nom", False, "", "Lorem Ipsum", ""],
+                                ["Nom2", False, "", "Lorem Ipsum2", ""],
+                                ["Nom3", False, "", "Lorem Ipsum3", ""]], 
+                           n=n, path="/ex/newton")
 
 if __name__ == '__main__':
     app.run()
